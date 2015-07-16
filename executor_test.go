4d41c702a0dea272f9ae336020e640ee410ecc5c
Prateek Srivastava <f2prateek@gmail.com>
2015-07-15 22:24:28 -0700

    Initial Commit

---
+++ executor_test.go
@@ -0,0 +1,24 @@
+package executor
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestExecutor(t *testing.T) {
+	e := NewPooledExecutor(20)
+
+	var wg sync.WaitGroup
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		e.enqueue(func() {
+			defer wg.Done()
+			time.Sleep(500 * time.Millisecond)
+			fmt.Println("hello")
+		})
+	}
+
+	wg.Wait()
+}
